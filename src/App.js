4ce53531a21560d8f1e7592dad2d8828c7999917
overfox-company <overfoxcompany@gmail.com>
2025-02-25 21:20:17 -0400

    add assets

--- src/App.js
+++ src/App.js
@@ -16,38 +16,36 @@ import {
 } from '@react-three/drei'
 import { TransformControls } from '@react-three/drei'
 import { LayerMaterial, Color, Depth } from 'lamina'
-import { Model } from './911-transformed'
 import { OrbitControls } from '@react-three/drei'
-const innerMaterial = new THREE.MeshStandardMaterial({
-  transparent: true,
-  opacity: 1,
-  color: 'black',
-  roughness: 0,
-  side: THREE.FrontSide,
-  blending: THREE.AdditiveBlending,
-  polygonOffset: true,
-  polygonOffsetFactor: 1,
-  envMapIntensity: 2
-})
+import { Car } from './Car'
+import { Worbench } from './Workbench'
+import { Shelf } from './Shelf'
+import { Box } from './Box'
+
 
 export default function App() {
   const [degraded, degrade] = useState(false)
 
   return (
-    <Canvas shadows camera={{ position: [5, 0, 15], fov: 30 }}>
-      <spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-      <ambientLight intensity={0.5} />
-      <Porsche scale={1.6} position={[-0.5, -0.18, 0]} rotation={[0, Math.PI / 5, 0]} />
-      <AccumulativeShadows position={[0, -1.16, 0]} frames={100} alphaTest={0.9} scale={10}>
-        <RandomizedLight amount={8} radius={10} ambient={0.5} position={[1, 5, -1]} />
-      </AccumulativeShadows>
+    <Canvas shadows camera={{ position: [5, 0, 15], fov: 60 }} style={{ backgroundColor: "rgb(88, 88, 116)" }}>
 
+
+      <ambientLight intensity={0.6} />
+      {/*<Porsche scale={1.6} position={[-0.5, -0.18, 0]} rotation={[0, Math.PI / 5, 0]} />
+      <AccumulativeShadows position={[-0.5, -0.18, 0]} frames={100} alphaTest={0.9} scale={10}>
+        <RandomizedLight amount={8} radius={10} ambient={0.5} position={[1, 5, -1]} />
+      </AccumulativeShadows>*/}
+      <Car />
+      <Worbench />
+      <Shelf />
+      <Box />
+      <AccumulativeShadows position={[0, -0.5, 0]} scale={20} frames={100} alphaTest={0.9}>
+        <RandomizedLight amount={8} radius={1} ambient={0.6} position={[1, 5, -1]} />
+      </AccumulativeShadows>
       {/** PerfMon will detect performance issues */}
       <PerformanceMonitor onDecline={() => degrade(true)} />
       {/* Renders contents "live" into a HDRI environment (scene.environment). */}
-      <Environment frames={degraded ? 1 : Infinity} resolution={256} background blur={1}>
-        <Lightformers />
-      </Environment>
+
       <OrbitControls />
       <CameraRig />
     </Canvas>
@@ -88,7 +86,7 @@ function Porsche(props) {
 }
 function Lightformers({ positions = [2, 0, 2, 0, 2, 0, 2, 0] }) {
   const group = useRef()
-  useFrame((state, delta) => (group.current.position.z += delta * 10) > 20 && (group.current.position.z = -60))
+  // useFrame((state, delta) => (group.current.position.z += delta * 10) > 20 && (group.current.position.z = -60))
   return (
     <>
       {/* Ceiling */}
